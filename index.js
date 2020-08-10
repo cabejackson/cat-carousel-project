7a5173b28a6423aa893a596c34ed13b370e7bec0
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2020-08-10 11:49:25 -0400

    finished drills

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 $('.thumbnail').on('click', e => {
   console.log(e.target);
+  //console.log(this);
+$("div.hero img").attr("src", $(e.target).attr("src"));
+$("div.hero img").attr("alt", $(e.target).attr("alt"));
+
+
+  
+
 });
+
+//$(".thumbnail").click(function() { $("#mainImage").attr("src", $(this).attr("src")); })
